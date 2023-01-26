961bcb5a588d1ea4b4576c0c31c55ee5f8f0ce2e
ABID_ANSARI <ansariabid123s@gmail.com>
2023-01-27 00:28:36 +0530

    modification in find 3 of max

--- practiceProblem.cpp/findMaxof3.cpp
+++ practiceProblem.cpp/findMaxof3.cpp
@@ -6,23 +6,26 @@ using namespace std;
 int main(){
 
 
-    int a,b,c;
+    int a,b,c; //user input
+    cout<<"Enter a value of A : ";
     cin>>a;
+    cout<<"Enter a value of B : ";
     cin>>b;
+    cout<<"Enter a value of C : ";
     cin>>c;
 
+    cout<<"Given Number are : "<<a<<","<<b<<","<<c<<endl;
     if (a>b)
     {
         if (a>c)
         {
-            cout<<"a is grater";
-        }else{
-            cout<<"c is greater";
+            cout<<a<<" -> a is greater";
         }
-        
+    }else if(b>c){
+        cout<<b<<" -> b is greater";
+    }else{
+        cout<<c<<" -> c is greater";
     }
     
-
-
     return 0;
 }
