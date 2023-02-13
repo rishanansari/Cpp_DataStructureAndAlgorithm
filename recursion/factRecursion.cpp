a15715084138adfc0cfe23eac1e04330363fa5cf
ABID_ANSARI <ansariabid123s@gmail.com>
2023-02-14 00:08:44 +0530

    factorial of a number by recursion

--- recursion/factRecursion.cpp
+++ recursion/factRecursion.cpp
@@ -1,1 +1,25 @@
 //factorial of a number by recursion
+// let n=5 ---> fact of number is 5x4x3x2x1
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+
+    if (n==1)
+    {
+        return 1;
+    }else{
+        return n*fact(n-1);
+    }
+    
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter the number to find fact : ";
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
